a603ec9573683e3600ac5671932d6ef1615cf524
Somya Jain <somyaj0308@gmail.com>
2026-05-04 11:59:13 +0530

    Create test_bank.py

---
+++ Phase-1/CS50p-Course/week 5/test_bank/test_bank.py
@@ -0,0 +1,29 @@
+"""
+In a file called bank.py, reimplement Home Federal Savings Bank from Problem Set 1, restructuring your code per the below, wherein value expects a str as input
+and returns an int, namely 0 if that str starts with “hello”, 20 if that str starts with an “h” (but not “hello”), or 100 otherwise, treating the str
+case-insensitively. You can assume that the string passed to the value function will not contain any leading spaces. Only main should call print.
+
+def main():
+    ...
+
+
+def value(greeting):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+Then, in a file called test_bank.py, implement three or more functions that collectively test your implementation of value thoroughly,
+each of whose names should begin with test_ so that you can execute your tests with:
+
+pytest test_bank.py
+
+"""
+def test_hello():
+    assert value("hello") == "$0"
+
+def test_h():
+    assert value("harry") == "$20"
+
+def test_noth():
+    assert value("world") == "$100"
